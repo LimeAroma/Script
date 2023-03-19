d68e85d3faef853145338cb2c093ff472b952804
Lime <59417283+LimeAroma@users.noreply.github.com>
2023-03-19 23:27:16 +0800

    Create PupuSplashAds.js

---
+++ Js/PupuSplashAds.js
@@ -0,0 +1,18 @@
+/*
+^https?:\/\/j1\.pupuapi\.com\/client\/marketing\/banner\/v7\?position_types=[A-Z0-9%]+&store_id=[a-z0-9\-]+ url script-response-body PupuSplashAds.js
+
+hostname=j1.pupuapi.com
+*/
+
+let obj=JSON.parse($response.body);
+let count = 0;
+obj.data.forEach(item => {
+  if (item.image_width === 1080) {
+    item.img_url = null;
+    count++;
+    if (count === 2) {
+      return false;
+    }
+  }
+});
+$done({body: JSON.stringify(obj)});
