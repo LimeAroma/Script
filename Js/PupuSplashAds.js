9847fbb6dc451a836a08c2e5e86ed26172cb8649
Lime <59417283+LimeAroma@users.noreply.github.com>
2023-03-21 21:28:33 +0800

    Update PupuSplashAds.js

--- Js/PupuSplashAds.js
+++ Js/PupuSplashAds.js
@@ -1,6 +1,6 @@
 // 作者：LimeAroma
 /*
-^https?:\/\/j1\.pupuapi\.com\/client\/marketing\/banner\/v7\?position_types=[A-Z0-9%]+&store_id=[a-z0-9\-]+ url script-response-body https://raw.githubusercontent.com/LimeAroma/Quantumultx/main/Js/PupuSplashAds.js
+https?:\/\/j1\.pupuapi\.com\/client\/marketing\/banner\/v7\?position_types=2(%[A-Z0-9]+)+&store_id=[a-z0-9\-]+ url script-response-body https://raw.githubusercontent.com/LimeAroma/Quantumultx/main/Js/PupuSplashAds.js
 
 hostname=j1.pupuapi.com
 */
