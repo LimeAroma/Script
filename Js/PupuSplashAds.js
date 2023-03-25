d642bcfff90f7f037193a0b0ab35a9955b468240
Lime <59417283+LimeAroma@users.noreply.github.com>
2023-03-25 18:43:18 +0800

    Update PupuSplashAds.js

--- Js/PupuSplashAds.js
+++ Js/PupuSplashAds.js
@@ -6,9 +6,5 @@ hostname=j1.pupuapi.com
 */
 
 let obj=JSON.parse($response.body);
-for (const item of obj.data) {
-  if (item.position_type === 50) {
-    item.img_url = null;
-  }
-}
+obj.data = obj.data.filter(item => !(item.position_type === 50));
 $done({body: JSON.stringify(obj)});
