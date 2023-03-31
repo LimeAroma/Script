f99856c4361935844960e20c86f05d860fa7bffd
Lime <59417283+LimeAroma@users.noreply.github.com>
2023-04-01 07:37:17 +0800

    Update PupuSplashAds.js

--- Js/PupuSplashAds.js
+++ Js/PupuSplashAds.js
@@ -2,13 +2,13 @@
 /*
 //Loon订阅脚本
 [Script]
-http-response https?:\/\/j1\.pupuapi\.com\/client\/marketing\/banner\/v7\?position_types=2(%[A-Z0-9]+)+&store_id script-path=https://raw.githubusercontent.com/LimeAroma/Quantumultx/main/Js/PupuSplashAds.js, requires-body=true, tag=朴朴超市开屏广告
+http-response https?:\/\/j1\.pupuapi\.com\/client\/marketing\/banner\/v7\?position_types=2(%[A-Z0-9]+)+&store_id script-path=https://raw.githubusercontent.com/LimeAroma/Script/main/Js/PupuSplashAds.js, requires-body=true, tag=朴朴超市开屏广告
 
 [MITM]
 hostname=j1.pupuapi.com
 
 //圈x远程引用
-https?:\/\/j1\.pupuapi\.com\/client\/marketing\/banner\/v7\?position_types=2(%[A-Z0-9]+)+&store_id url script-response-body https://raw.githubusercontent.com/LimeAroma/Quantumultx/main/Js/PupuSplashAds.js
+https?:\/\/j1\.pupuapi\.com\/client\/marketing\/banner\/v7\?position_types=2(%[A-Z0-9]+)+&store_id url script-response-body https://raw.githubusercontent.com/LimeAroma/Script/Js/PupuSplashAds.js
 
 hostname=j1.pupuapi.com
 */
