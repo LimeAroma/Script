c8028cf30bd2c6bd82001ec20ad2595d5796ee71
Lime <59417283+LimeAroma@users.noreply.github.com>
2023-04-01 07:33:41 +0800

    Create BiliMangaFeeds.js

---
+++ Js/BiliMangaFeeds.js
@@ -0,0 +1,18 @@
+// 作者：LimeAroma
+/*
+//Loon订阅脚本
+[Script]
+^https?:\/\/manga\.bilibili\.com\/twirp\/comic\.v\d\.Home\/HomeFeed script-path=https://raw.githubusercontent.com/LimeAroma/Script/main/Js/BiliMangaFeeds.js, requires-body=true, tag=哔漫信息流周边推荐
+
+[MITM]
+hostname=j1.pupuapi.com
+
+//圈x远程引用
+^https?:\/\/manga\.bilibili\.com\/twirp\/comic\.v\d\.Home\/HomeFeed url script-response-body https://raw.githubusercontent.com/LimeAroma/Script/main/Js/BiliMangaFeeds.js
+
+hostname=manga.bilibili.com
+*/
+
+let obj=JSON.parse($response.body);
+obj.data.feeds = obj.data.feeds.filter(item => !(item.type === 15||item.type === 30));
+$done({body: JSON.stringify(obj)});
